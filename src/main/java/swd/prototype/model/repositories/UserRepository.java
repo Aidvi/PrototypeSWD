ace1afcf3e35fdf63653d344065b4408f1298dac
Aidvi <nissen.magnus@outlook.dk>
2018-06-13 17:43:46 +0200

    Login med session working

--- src/main/java/swd/prototype/model/repositories/UserRepository.java
+++ src/main/java/swd/prototype/model/repositories/UserRepository.java
@@ -17,18 +17,7 @@ public class UserRepository {
 
     @Autowired
     private NamedParameterJdbcTemplate template;
-
-
-    public User readUser(User currentUser){
-
-        String sql = "SELECT * FROM user WHERE username ='" + currentUser.getUsername() + "' AND password =  '"+ currentUser.getPassword() + "'";
-
-        SqlParameterSource namedParameters = new MapSqlParameterSource("username", String.valueOf(currentUser.getUsername()));
-        User user = (User)template.queryForObject(sql, namedParameters, new userRowMapper());
-
-
-        return user;
-    }
+    
     public User findUser(User currentUser){
 
         String sql = "SELECT * FROM user WHERE username = :username AND password = :password";
